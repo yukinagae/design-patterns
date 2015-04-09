5f931e81a4056076ab6d024c3e13e637ad524a28
yukinagae <yuki.nagae1130@gmail.com>
2015-04-09 10:11:33 +0900

    add Basic Constructors

---
+++ constructor-pattern/basic-constructors.js
@@ -0,0 +1,17 @@
+/**
+* Basic Constructors
+*/
+function Car(model, year, miles) {
+  this.model = model;
+  this.year = year;
+  this.miles = miles;
+  this.toString = function() {
+    return this.model + " has done " + this.miles + " miles.";
+  }
+}
+
+var civic = new Car("Honda Civic", 2009, 20000);
+var mondeo = new Car("Ford Mondeo", 2010, 5000);
+
+console.log(civic.toString());
+console.log(mondeo.toString());
